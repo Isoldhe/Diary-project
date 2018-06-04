b69a24658fb504e1fbea6eb3824424b552290268
Linda de Haan <linda88haan@gmail.com>
2018-06-04 14:28:02 +0200

    - Server User model toegevoegd - UserController toegevoegd

--- server/src/main/java/nl/iamlinda/server/controllers/UserController.java
+++ server/src/main/java/nl/iamlinda/server/controllers/UserController.java
@@ -1,4 +1,14 @@
 package nl.iamlinda.server.controllers;
 
+import nl.iamlinda.server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 }
