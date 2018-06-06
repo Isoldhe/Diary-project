a7460d1b6c5b91bed27d5e920c35d78e8679c662
Linda de Haan <linda88haan@gmail.com>
2018-06-06 11:16:10 +0200

    - PostdetailComponent toegevoegd (voor read only pages van 1 post) - Routings toegevoegd

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {NewpostComponent} from "./newpost/newpost.component";
 import {PostListComponent} from "./post-list/post-list.component";
+import {PostDetailComponent} from "./post-detail/post-detail.component";
 
 // We still need to add more routes and edit the components, so they refer to the correct paths
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'newpost', component:  NewpostComponent},
-  // { path: 'post/:id', component:  PostDetailComponent},  // TODO: generate this component, shows one post after clicking it on homepage
+  { path: 'postdetail/:id', component:  PostDetailComponent},
+  // { path: 'editpost/:id', component:  EditpostComponent},
   { path: '', component: PostListComponent },
 
   // otherwise redirect to home (PostListComponent)
